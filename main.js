b5a5d8b7313de76e6bf58c685a1a181ad6145eb6
AVINASH VERMA <avinashvermamth19@gmail.com>
2021-02-23 00:02:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+
+            document.getElementById("div-1").onmouseenter = function(){
+
+             var aa = document.getElementById("div-1").innerHTML;
+
+             document.getElementById("img-main").innerHTML = aa;
+             
+
+            }
+
+
+            document.getElementById("div-2").onmouseenter = function(){
+
+                var bb = document.getElementById("div-2").innerHTML;
+   
+                document.getElementById("img-main").innerHTML = bb;
+                
+   
+               }
+
+               document.getElementById("div-3").onmouseenter = function(){
+
+                var cc = document.getElementById("div-3").innerHTML;
+   
+                document.getElementById("img-main").innerHTML = cc;
+                
+   
+               }
+
+
+               document.getElementById("div-4").onmouseenter = function(){
+
+                var dd = document.getElementById("div-4").innerHTML;
+   
+                document.getElementById("img-main").innerHTML = dd;
+                
+   
+               }
+
+              
+
+
+               
